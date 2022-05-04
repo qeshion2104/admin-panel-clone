ff17110f7d05a1f21e575f5a1fbcacba928d9a3c
Allen.hao <Allen.hao@roycetechnology.com>
2022-05-04 16:28:28 +0200

    feat: finish all template

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
-import "./Card.css"
-import { AnimateSharedLayout } from 'framer-motion'
-import CompactCard from './CompactCard';
-import ExpandedCard from './ExpandedCard';
+import React, { useState } from "react";
+import "./Card.css";
+import { AnimateSharedLayout } from "framer-motion";
+import CompactCard from "./CompactCard";
+import ExpandedCard from "./ExpandedCard";
 const Card = (props) => {
-    const [isExpanded, setIsExpanded] = useState(false);
-    return (
-        <div className="Card">
-            <AnimateSharedLayout>
-                {
-                    isExpanded 
-                        ? <ExpandedCard {...props} toggleExpand={() => setIsExpanded(false)}/> 
-                        : <CompactCard {...props} toggleExpand={() => setIsExpanded(true)}/>
-                }
-            </AnimateSharedLayout>
-        </div>
-    )
-}
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <div className="Card">
+      <AnimateSharedLayout>
+        {isExpanded ? (
+          <ExpandedCard {...props} toggleExpand={() => setIsExpanded(false)} />
+        ) : (
+          <CompactCard {...props} toggleExpand={() => setIsExpanded(true)} />
+        )}
+      </AnimateSharedLayout>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,44 +1,85 @@
-import React, { useState } from 'react'
-import "./Sidebar.css"
-import Logo from "imgs/logo.png"
-import { sidebarData } from "Data"
-
+import React, { useState } from "react";
+import "./Sidebar.css";
+import Logo from "imgs/logo.png";
+import { sidebarData } from "Data";
+import { UilBars } from "@iconscout/react-unicons";
+import { motion } from 'framer-motion'
 
 const Sidebar = () => {
-    const [selectedIndex, setSelectedIndex] = useState(0)
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [expanded, setExpanded] = useState(true);
+  const menuBarVarient = {
+    "expanded": {
+      left: "70%"
+    },
+    "unExpanded": {
+      left: "5%"
+    }
+  }
+  const sidebarVarient = {
+      "expanded": {
+        left: "0%"
+      },
+      "unExpanded": {
+        left: "-75%"
+      },
+      "nonMobile": {
+        left: "0%"
+      }
+  }
+
+  const isMobileScreen = window.innerWidth <= 768
 
   return (
-    <div className='Sidebar'>
+    <>
+      {/* mobile menu icon */}
+      <motion.div
+        className={`Sidebar__menuBar ${
+          expanded
+            ? "Sidebar__menuBar--expanded"
+            : "Sidebar__menuBar--unExpanded"
+        }`}
+        animate={expanded? "expanded" : "unExpanded"}
+        variants={menuBarVarient}
+        onClick={() => setExpanded(!expanded)}
+      >
+        <UilBars />
+      </motion.div>
+      <motion.div className="Sidebar"
+        variants={sidebarVarient}
+        animate={ isMobileScreen ? ( expanded ? "expanded" : "unExpanded" ) : "nonMobile"}
+      >
         {/* Logo */}
         <div className="Sidebar__logo">
-            <img src={Logo} alt="logo" />
-            <span>
-                Sh
-                <span>o</span>
-                ps
-            </span>
+          <img src={Logo} alt="logo" />
+          <span>
+            Sh
+            <span>o</span>
+            ps
+          </span>
         </div>
         {/* MenuItems */}
         <div className="Sidebar__menu">
-            {
-                sidebarData.map((item, index) => {
-                    const className = selectedIndex == index ? "Sidebar__menuItem Sidebar__menuItem--active" : "Sidebar__menuItem"
-                    return (
-                        <div 
-                            key={index} 
-                            className={className}
-                            onClick={() => setSelectedIndex(index)}
-                        >
-                            <item.icon/>
-                            <span className='Sidebar__heading'>{item.heading}</span>
-                        </div>
-                    )
-                })
-            }
-        </div> 
+          {sidebarData.map((item, index) => {
+            const className =
+              selectedIndex == index
+                ? "Sidebar__menuItem Sidebar__menuItem--active"
+                : "Sidebar__menuItem";
+            return (
+              <div
+                key={index}
+                className={className}
+                onClick={() => setSelectedIndex(index)}
+              >
+                <item.icon />
+                <span className="Sidebar__heading">{item.heading}</span>
+              </div>
+            );
+          })}
+        </div>
+      </motion.div>
+    </>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Sidebar
+export default Sidebar;
